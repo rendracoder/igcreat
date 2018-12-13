5d4193fbd360b53461367cb9a83db269ed160ac8
Nathanael Ferry P <ntnferry1505@gmail.com>
2018-12-13 14:54:00 +0700

    Update ids.php

--- ids.php
+++ ids.php
@@ -2,36 +2,51 @@
 require('function.php');
 @set_time_limit(0);
 @clearstatcache();
-@ini_set('max_execution_time',0);
-@ini_set('output_buffering',0);
+@ini_set('max_execution_time', 0);
+@ini_set('output_buffering', 0);
 
-echo "* Kosongkan proxy jika tidak memakai proxy \n* Jumlah maksimal 10 \n\n";
+echo banner()."\n";
 
-$acak       = 'axegram'.acak('1234567890', '5').'';
-$username   = $acak;
-$password   = 'helloaxeeeee';
-$email      = ''.$acak.'@theaxelab.tech';
-$name       = 'Si '.$acak.'';
-echo "Insert your proxy : ";
-$proxy      = trim(fgets(STDIN, 1024));
-// $proxy      = '159.69.2.199:80';
-$date       = date("Y-m-d");
-$file       = 'result.html';
-echo "Count : ";
-$count      = trim(fgets(STDIN, 1024));
-
-echo "Please wait... \n\n";
-for ($i=0; $i < $count; $i++) {
-	$create     = create($username, $password, $email, $name, $proxy);
-	$data       = json_decode($create);
-	if($data->account_created == 'true'){
-		$file   = fopen(''.$file.'', 'r+') or die("file not found!");;
-		$get    = fgets($file);
-		$catat  = fwrite($file, ''.$username.':'.$password.'<br>');
-		fclose($file);
-		echo "".$i." Success create ".$username.":".$password."\n";
-	} else {
-		echo "".$i." Failed create ".$username.":".$password."\n";
-	}
-	sleep(10);
+while (true) {
+    $ua         = 'Mozilla/5.0 (Linux; Android 5.1.1; Redmi Note 3 Build/LMY47V) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36';
+    $proses     = proccessmail('https://10minutemail.net/m/?lang=id', $ua);
+    preg_match_all('%Set-Cookie: (.*?);%', $proses[0], $d);
+    $cookie     = '';
+    for ($o = 0; $o < count($d[0]); $o++)
+        $cookie .= $d[1][$o] . ";";
+    $sesi       = explode('PHPSESSID=', $cookie);
+    $sesi       = explode(';', $sesi[1]);
+    $sesi       = $sesi[0];
+    $time       = strtotime(date('Y-m-d H:i:sP'));
+    ////----////----////----////----////----////
+    $getlist    = proccessmail('https://10minutemail.net/address.api.php?sessionid=' . $sesi . '&_=' . $time, $ua, $cookie);
+    $getlist    = json_decode($getlist[1], true);
+    $data       = json_decode(file_get_contents('https://randomuser.me/api/'), true);
+    $username   = $data['results'][0]['login']['username'];
+    $password   = $data['results'][0]['login']['password'].''.$data['results'][0]['name']['first'];
+    $email      = $getlist['mail_get_mail'];
+    $name       = ucfirst($data['results'][0]['name']['first']);
+    //echo "Insert your proxy : ";
+    //$proxy      = trim(fgets(STDIN, 1024));
+    $proxy      = '95.216.1.195:80';
+    $proxypwd   = 'gupibtat-3:0uf064rmznlu';
+    $date       = date("Y-m-d");
+    $file       = 'result.txt';
+    $create     = create($username, $password, $email, $name, $proxy, $proxypwd);
+    $data       = json_decode($create, true);
+    if($data['account_created'] == true){
+        $file  = fopen($file, 'r+') or die("file not found!");
+        $get   = fgets($file);
+        $catat = fwrite($file, "" . $username . ":" . $password . "\n");
+        fclose($file);
+        echo "Success create " . $username . ":" . $password . "\n";
+    } else if($data['errors']['ip']){
+        echo $data['errors']['ip'][0]."\n";
+    } else if($data['errors']['username']){
+        echo $data['errors']['username'][0]['message']."\n";
+    } else {
+        echo "Unknown error\n";
+    }
+    sleep(100);
 }
+?>
